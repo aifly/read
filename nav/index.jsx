e4bab533e4cfa062691a04834e466beff40a16e9
clearinfo <songxian@163.com>
2017-05-23 14:49:59 +0800

    read

--- nav/index.jsx
+++ nav/index.jsx
@@ -18,7 +18,13 @@ class ZmitiNavApp extends Component {
 
 		return (
 			<div className='zmiti-nav-main-ui'>
-			ads	start1111111
+				<div className="zmiti-nav-top01"></div>
+				<div className="zmiti-nav-btn01">
+					<button onClick={this.openlink.bind(this)}>直接报名</button>
+					<button>地方报名</button>
+				</div>
+				<div className="zmiti-nav-bot01"></div>
+
 			</div>
 		);
 	}
@@ -26,6 +32,9 @@ class ZmitiNavApp extends Component {
 
 	componentDidMount() {
 
+	}
+	openlink(){
+		window.location="http://cn.mikecrm.com/7cDhYqu?from=groupmessage";
 	}
 }
 export default PubCom(ZmitiNavApp);
